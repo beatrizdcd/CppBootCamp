876c7ec8205f00b0a9c5e98a4e9a5c7662831bbc
beatrizdcd <beatriz.de.castro.diez@volvocars.com>
2022-04-05 15:22:04 +0200

    Updated MiniProject

--- Assigments/MiniProject/MiniProject_BookShop.cpp
+++ Assigments/MiniProject/MiniProject_BookShop.cpp
@@ -46,6 +46,7 @@ void Book::printMenu (){
     cin >> choice;
     cin.ignore();
 
+
     switch (choice) {
         case 1:
             newBook();
@@ -80,13 +81,18 @@ void Book::newBook(){
     cout<<  "- Enter Publisher Name:  ";
     getline(cin, publisher);
     cout<<  "- Enter Price:  ";
+    // check that price typevariable is correct
     while (!(cin>>price)){
         cout<< "Not valid value. Please, enter a number.";
         cin.clear();
         cin.ignore();
     }
     cout<<  "- Enter Number of Copies:  ";
-    cin >> stock;
+    while (!(cin>>stock)){
+        cout<< "Not valid value. Please, enter a number.";
+        cin.clear();
+        cin.ignore();
+    }
     cout <<endl;
 }
 
